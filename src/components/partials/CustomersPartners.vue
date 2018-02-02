6ef7cc6676af01586c97e1a5a6ba4632ea2d6bf8
Pixeldenker <bram@pixeldenker.nl>
2018-02-02 12:53:41 +0100

    Added customers and partners block

--- src/components/partials/CustomersPartners.vue
+++ src/components/partials/CustomersPartners.vue
@@ -2,6 +2,18 @@
 <div class="customers-partners">
     <h2 class="dark center">Klanten en partners waar wij trots op zijn</h2>
     <p class="dark center">Hey, mijn naam is Rob Scholten. Een passievolle freelancer, een pixeldenker. Sinds 2015 werk ik aan projecten die ik leuk en inspirerend vind. Als ambitieuze jonge creatieveling met een passie voor digital design werk ik nauw samen met mijn partners. </p>
+
+    <div class="partners">
+        <div class="partner"></div>
+        <div class="partner"></div>
+        <div class="partner"></div>
+        <div class="partner"></div>
+
+        <div class="partner"></div>
+        <div class="partner"></div>
+        <div class="partner"></div>
+        <div class="partner"></div>
+    </div>
 </div>
 </template>
 
@@ -21,6 +33,22 @@ export default {
     p {
         max-width: 800px;
         margin: 16px auto 0;
+    }
+
+    .partners {
+        max-width: 980px;
+        margin: 120px auto 0;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-template-rows: auto;
+        grid-column-gap: 1px;
+
+        .partner {
+            width: 100%;
+            height: 32px;
+            background-color: gray;
+            margin-bottom: 115px;
+        }
     }
 }
 </style>
