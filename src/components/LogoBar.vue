a3b3c3b786ecab06daa5ea1b364cdbb32b254a4d
Pixeldenker <bram@pixeldenker.nl>
2018-04-23 22:28:03 +0200

    Revert "Revert "Fixed bugs""
    This reverts commit 13f19a939f28fb2f810dc30a4300f5f3cf3a7a32.

--- src/components/LogoBar.vue
+++ src/components/LogoBar.vue
@@ -126,6 +126,7 @@ export default {
             @include transition(all, 500ms);
 
             .hamburger-menu {
+                // top: 84px;
                 margin-top: 55px;
                 @include transition(all, 500ms);
             }
@@ -139,6 +140,8 @@ export default {
                 margin-top: 0;
 
                 .hamburger-menu {
+                    // position: fixed;
+                    // top: 0px;
                     margin-top: 0;
                 }
             }
